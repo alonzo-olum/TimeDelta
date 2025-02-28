6547e5a02c00158544ee58355c92f57fe239c10a
alonzo olum <alondiff@gmail.com>
2025-02-28 16:32:41 +0300

    update: logic flow fixes

--- fix.go
+++ fix.go
@@ -13,16 +13,20 @@ func Fix(in []Point, start, end, interval uint32) []Point {
 	var result []Point = make([]Point, 0)
 	for _, i := range in {
 
-		var index int
 		if start < i.Ts {
-			index = shiftIndex(start, i.Ts, interval)
-			result = append(result, shiftLeading(index, start, interval)...)
+			index := shiftIndex(start, i.Ts, interval)
+			result = append(result, shift(index, start, interval)...)
 		}
 		if start == i.Ts {
-			index = shiftIndex(start, end, interval)
-			result = append(result, shiftLagging(i, index, start, interval)...)
+			result = append(result, i)
 		}
 		start += interval
+	}
+
+	index := shiftIndex(start, end, interval)
+	// are there missing points from last point to end
+	if index > 1 {
+		result = append(result, shift(index, start, interval)...)
 	}
 	return result
 }
@@ -31,7 +35,7 @@ func shiftIndex(start, end, delta uint32) int {
 	return int((end - start) / delta)
 }
 
-func shiftLeading(index int, start, delta uint32) []Point {
+func shift(index int, start, delta uint32) []Point {
 	var result = make([]Point, 0)
 	var offset = start
 
@@ -43,16 +47,8 @@ func shiftLeading(index int, start, delta uint32) []Point {
 	return result
 }
 
-func shiftLagging(ref Point, index int, start, delta uint32) []Point {
-	var result = make([]Point, 0)
-	var offset = start
-	for idx := 0; idx < index; idx++ {
-		if offset == ref.Ts {
-			result = append(result, ref)
-		} else {
-			result = append(result, Point{Val: math.NaN(), Ts: offset})
-		}
-		offset += delta
-	}
-	return result
+func roundToNearest10(delta uint32) uint32 {
+	var ones float64 = float64(delta) / 10
+	roundedFloat := math.Round(ones) * 10
+	return math.Float32bits(float32(roundedFloat))
 }
